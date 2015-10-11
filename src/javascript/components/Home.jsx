dc2e882811c7d5b45d77f223257a9f5792a1cace
J Scott Smith <jscsmith@gmail.com>
2015-10-11 00:05:02 -0700

    display latest release for `Home` route

--- src/javascript/components/Home.jsx
+++ src/javascript/components/Home.jsx
@@ -2,13 +2,36 @@
 
 import React from 'react';
 
+// Action
+import getRelease from '../actions/getRelease';
+
 export default class Home extends React.Component{
 
-    render () {
+	componentWillMount () {
+        this.context.executeAction(getRelease, 'LATEST');
+    }
+
+    renderLoading () {
         return (
             <div>
-                <h1>Welcome to react-flux-gulp-starter!</h1>
+                <h1>loading</h1>
             </div>
         );
     }
+
+    renderRelease () {
+        return (
+            <article>
+                <h1>{this.props.latest.title}</h1>
+            </article>
+        );
+    }
+
+    render () {
+        return typeof this.props.latest !== 'undefined' ? this.renderRelease() : this.renderLoading();
+    }
 }
+
+Home.contextTypes = {
+    executeAction: React.PropTypes.func.isRequired
+};
