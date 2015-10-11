6568a99e82ee036539b752a5251d103f14cb494d
J Scott Smith <jscsmith@gmail.com>
2015-10-10 20:13:02 -0700

    properly name `getReleases` action

--- src/javascript/actions/getReleases.js
+++ src/javascript/actions/getReleases.js
@@ -1,6 +1,6 @@
 'use strict';
 
-export default function navigate (actionContext, payload, done) {
+export default function getReleases (actionContext, payload, done) {
     actionContext.dispatch('GET_RELEASES', payload);
     done();
 }
