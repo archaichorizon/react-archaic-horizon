739eee10638062f188265ff76c49f21638402ba2
J Scott Smith <jscsmith@gmail.com>
2015-10-17 11:21:39 -0700

    update `Release` when receiving new props `routeParams`

--- src/javascript/components/pages/Release.jsx
+++ src/javascript/components/pages/Release.jsx
@@ -10,8 +10,19 @@ import getRelease from '../../actions/getRelease';
 
 class Release extends React.Component {
 
-	componentWillMount () {
-		let catNo = this.props.routeParams.catNo;
+    componentWillMount () {
+        const catNo = this.props.routeParams.catNo;
+        this.fetchRelease(catNo);
+    }
+
+    componentWillReceiveProps (nextProps) {
+        const catNo = nextProps.routeParams.catNo;
+        if (nextProps.routeParams.catNo !== this.props.routeParams.catNo) {
+            this.fetchRelease(catNo);
+        }
+    }
+
+    fetchRelease (catNo) {
         this.context.executeAction(getRelease, catNo);
     }
 
