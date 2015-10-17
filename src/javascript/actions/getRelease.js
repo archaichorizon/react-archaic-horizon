5ea6808011d719b592565e776174fd46d2bab04d
J Scott Smith <jscsmith@gmail.com>
2015-10-17 11:18:56 -0700

    update actions, add caching

--- src/javascript/actions/getRelease.js
+++ src/javascript/actions/getRelease.js
@@ -2,9 +2,21 @@
 
 import request from'superagent-bluebird-promise';
 
+// set release to its catalog 
+let cache = {};
+
 export default function getRelease (actionContext, payload, done) {
     
     let url;
+    payload = payload.toUpperCase();
+
+    // check cache and return if true
+    if(cache[payload]) {
+    	// console.log('Cached Response');
+    	actionContext.dispatch('GET_RELEASE', cache[payload]);
+    	done();
+    	return;
+    }
 
     switch (payload) {
         case 'LATEST':
@@ -17,11 +29,17 @@ export default function getRelease (actionContext, payload, done) {
 	
 	return request
 		.get(url).then( function(res) {
-			console.log(res);
+			// console.log(res);
 			payload = res.body;
+
+			// cache the response
+		    let key = payload.cat_no;
+		    cache[key] = payload;
+
 			actionContext.dispatch('GET_RELEASE', payload);
 			done();
 		}, function(err) {
 			console.warn(err);
+			done();
 		});
 }
--- src/javascript/actions/getReleases.js
+++ src/javascript/actions/getReleases.js
@@ -4,14 +4,15 @@ import request from'superagent-bluebird-promise';
 
 export default function getReleases (actionContext, payload, done) {
 
-	let url = 'http://api.archaichorizon.com/releases.json';
+	const url = 'http://api.archaichorizon.com/releases.json';
 	
 	return request
 		.get(url).then( function(res) {
-			console.log(res);
+			// console.log(res);
 			actionContext.dispatch('GET_RELEASES', res.body);
 			done();
 		}, function(err) {
 			console.warn(err);
+			done();
 		});
 }
