b85edad843d6c37671ad83187c9ee29e1f0395fb
J Scott Smith <jscsmith@gmail.com>
2015-10-17 11:19:28 -0700

    add `ReleaseNavStore`

--- src/javascript/stores/ReleaseNavStore.js
+++ src/javascript/stores/ReleaseNavStore.js
@@ -1,31 +1,46 @@
 'use strict';
 
 import BaseStore from 'fluxible/addons/BaseStore';
-import ReleasesStore from '../stores/ReleasesStore';
+import _ from 'lodash';
 
 class ReleaseNavStore extends BaseStore {
     constructor (dispatcher) {
         super(dispatcher);
-        this.releaseNav = {
-            next: 'foo',
-            prev: 'foo'
-        };
+        this.nav = {};
+        this.nav.next;
+        this.nav.prev;
     }
 
-    handleNextRelease () {
-        this.getReleases();
+    handleGetReleases (releases) {
+        this.releases = releases;
     }
 
-    handlePrevRelease () {
-
+    handleGetRelease (release) {
+        this.current = release.cat_no;
+        this.setNextPrev(this.current);
     }
 
-    getReleases () {
-        this.releases = ReleasesStore.getState();
+    handleNextRelease (payload) {
+        this.setNextPrev(payload.next);
+    }
+
+    handlePrevRelease (payload) {
+        this.setNextPrev(payload.prev);
+    }
+
+    setNextPrev (current) {
+        let index = current ? _.findIndex(this.releases, {cat_no: current}) : null;
+        current = index || 0;
+
+        let releases = this.releases.length;
+        this.nav.prev = releases - 1 !== current ? this.releases[current+1].cat_no : null;
+        this.nav.next = current !== 0 ? this.releases[current-1].cat_no : null;
+        this.emitChange();
+
     }
 
     getState () {
-        return this.releaseNav;
+        return this.nav;
     }
 
     dehydrate () {
@@ -33,14 +48,16 @@ class ReleaseNavStore extends BaseStore {
     }
 
     rehydrate (state) {
-        this.releaseNav = state.releaseNav
+        this.nav = state.nav;
     }
 }
 
 ReleaseNavStore.storeName = 'ReleaseNavStore';
 ReleaseNavStore.handlers = {
+    'GET_RELEASES': 'handleGetReleases',
+    'GET_RELEASE': 'handleGetRelease',
     'NEXT_RELEASE': 'handleNextRelease',
-    'PREV_RELEASE': 'handlePrevRelease'
+    'PREV_RELEASE': 'handlePrevRelease',
 };
 
 export default ReleaseNavStore;
