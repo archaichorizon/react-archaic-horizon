78ebff143f468ae85b7c1ed83032202d560a473a
J Scott Smith <jscsmith@gmail.com>
2015-10-21 21:59:15 -0700

    refactor `AudioPlayer` to es6

--- src/javascript/components/audioPlayer/AudioPlayer.jsx
+++ src/javascript/components/audioPlayer/AudioPlayer.jsx
@@ -1,131 +1,102 @@
-var React = require('react/addons');
-var ButtonPanel = require("./ButtonPanel.jsx");
-var ProgressBar = require("./ProgressBar.jsx");
-var VolumeBar = require("./VolumeBar.jsx");
-var TimeLabel = require("./TimeLabel.jsx");
-var NameLabel = require("./NameLabel.jsx");
-var SongList = require("./SongList.jsx");
-// var AppPlayerStore = require('../../stores/AppPlayerStore');
-var Howl = require('../../vendor/howler.core').Howl;
+import React from 'react/addons';
+import ButtonPanel from './ButtonPanel.jsx';
+import ProgressBar from './ProgressBar.jsx';
+import VolumeBar from './VolumeBar.jsx';
+import TimeLabel from './TimeLabel.jsx';
+import NameLabel from './NameLabel.jsx';
+import SongList from './SongList.jsx';
+// import AppPlayerStore from '../../stores/AppPlayerStore';
+import { Howl } from '../../vendor/howler.core';
 
-var AudioPlayer = React.createClass({
+class AudioPlayer extends React.Component {
 
-	getInitialState:function() {
-		return {
+	constructor (props) {
+		super(props);
+		this.state = {
 			isPlaying: false,
 			isPause: false,
 			isLoading: false,
 			currentSongIndex: -1,
-			volume: 0.8
+			volume: 0.5
 		};
-	},
 
-	componentWillMount: function() {
-        // AppPlayerStore.addChangeListener(this._onStoreChange);
+		this.getSongName = this.getSongName.bind(this);
+		this.onPlayBtnClick = this.onPlayBtnClick.bind(this);
+		this.onPauseBtnClick = this.onPauseBtnClick.bind(this);
+		this.onPrevBtnClick = this.onPrevBtnClick.bind(this);
+		this.onNextBtnClick = this.onNextBtnClick.bind(this);
+		this.onSongItemClick = this.onSongItemClick.bind(this);
 
+		this.play = this.play.bind(this);
+		this.initSoundObject = this.initSoundObject.bind(this);
+		this.initSoundObjectCompleted = this.initSoundObjectCompleted.bind(this);
+		this.clearSoundObject = this.clearSoundObject.bind(this);
+		
+		this._play = this._play.bind(this);
+		this.playEnd = this.playEnd.bind(this);
+		this.stop = this.stop.bind(this);
+		this.pause = this.pause.bind(this);
+		this.prev = this.prev.bind(this);
+		this.next = this.next.bind(this);
+
+		this.updateSongIndex = this.updateSongIndex.bind(this);
+		this.updateCurrentDuration = this.updateCurrentDuration.bind(this);
+		this.stopUpdateCurrentDuration = this.stopUpdateCurrentDuration.bind(this);
+		this.seekTo = this.seekTo.bind(this);
+		this.adjustVolumeTo = this.adjustVolumeTo.bind(this);
+		this.songCount = this.songCount.bind(this);
+		this.getCurrentSongName = this.getCurrentSongName.bind(this);
+	}
+
+	componentWillMount () {
 		if (this.props.playlist) {
 			this.setState({ 
 				songs: this.props.playlist,
 				currentSongIndex: 0 
 			});
 		} else {
-			throw "no data";
+			throw "No playlist";
 		}
-	},
+	}
 
-	_onStoreChange: function() {
-		// console.log('change');
-    },
-
-	componentDidUpdate: function(prevProps, prevState, prevContext) {
+	componentDidUpdate (prevProps, prevState, prevContext) {
 		if (this.state.isPlaying && this.state.currentSongIndex != prevState.currentSongIndex) {
 			this.initSoundObject();
 		}
-	},
+	}
 
-	render: function() {
-
-		var songCount = this.songCount();
-		var percent = 0;
-		if (this.state.seek && this.state.duration) {
-			percent = this.state.seek / this.state.duration;
-		}
-
-		var songName;
-		if (this.songCount() > 1) {
-			var songList = (
-				<SongList ref="songList" 
-					songs={this.state.songs}
-					currentSongIndex={this.state.currentSongIndex} 
-					isPlaying={this.state.isPlaying} isPause={this.state.isPause} 
-					onSongItemClick={this.onSongItemClick}/>
-			);
-			songName = (this.state.currentSongIndex + 1) + ". " + this.getCurrentSongName();
-		} else {
-			songName = this.getCurrentSongName();
-		}
-		
-		return (
-			<div className="audio-player">		
-				<div className="audio-desc-container clearfix">
-					<NameLabel name={songName} />
-					<TimeLabel seek={this.state.seek} duration={this.state.duration}/>
-				</div>
-				<div className="clearfix">
-					<ButtonPanel isPlaying={this.state.isPlaying} 
-						isPause={this.state.isPause}
-						isLoading={this.state.isLoading}
-						currentSongIndex={this.state.currentSongIndex} 
-						songCount={songCount}
-						onPlayBtnClick={this.onPlayBtnClick} 
-						onPauseBtnClick={this.onPauseBtnClick}
-						onPrevBtnClick={this.onPrevBtnClick} 
-						onNextBtnClick={this.onNextBtnClick} />
-					<ProgressBar 
-						shorter={songCount > 1} 
-						percent={percent} 
-						seekTo={this.seekTo} />
-					<VolumeBar 
-						volume={this.state.volume} 
-						adjustVolumeTo={this.adjustVolumeTo} />
-					{/*songList*/}
-				</div>
-			</div>
-		);
-	},
-
-	getSongName: function(song) {
+	getSongName (song) {
 		if (song.hasOwnProperty("title")) {
 			return song.title;
 		} else {
 			return "Untitled";
 		}
-	},
+	}
 
-	onPlayBtnClick: function() {
+	onPlayBtnClick () {
 		if (this.state.isPlaying && !this.state.isPause) {
 			return;
 		}
 		this.play();
-	},
+	}
 
-	onPauseBtnClick: function() {
-		var isPause = !this.state.isPause;
+	onPauseBtnClick () {
+		const isPause = !this.state.isPause;
 		this.setState({ 
 			isPause: isPause 
 		});
 		isPause ? this.pause() : this._play();
-	},
+	}
 
-	onPrevBtnClick: function() {
+	onPrevBtnClick () {
 		this.prev();
-	},
+	}
 
-	onNextBtnClick: function() {
+	onNextBtnClick () {
 		this.next();
-	},
+	}
 
- 	onSongItemClick: function(songIndex) {
+ 	onSongItemClick (songIndex) {
  		// handle pause/playing state.
  		if (this.state.currentSongIndex == songIndex) {
  			if (this.state.isPause) {
@@ -148,10 +119,9 @@ var AudioPlayer = React.createClass({
 			isPause: false
 		});
  		// this.refs.songList.hideDropdownMenu();
+ 	}
 
- 	},
-
-	play: function() {
+	play () {
 		this.setState({ 
 			isPlaying: true, 
 			isPause: false 
@@ -160,8 +130,8 @@ var AudioPlayer = React.createClass({
 		if (!this.howler) {
 			this.initSoundObject();
 		} else {
-			var songUrl = encodeURI(this.state.songs[this.state.currentSongIndex].stream);
-			var howlerSongUrl = this.howler._src;
+			const songUrl = encodeURI(this.state.songs[this.state.currentSongIndex].stream);
+			const howlerSongUrl = this.howler._src;
 
 			if (songUrl != howlerSongUrl) {
 				this.initSoundObject();
@@ -169,15 +139,15 @@ var AudioPlayer = React.createClass({
 				this._play();
 			}
 		}
-	},
+	}
 
-	initSoundObject: function() {
+	initSoundObject () {
 		this.clearSoundObject();
 		this.setState({ 
 			isLoading: true 
 		});
 
-		var song = this.state.songs[this.state.currentSongIndex];
+		const song = this.state.songs[this.state.currentSongIndex];
 		this.howler = new Howl({
 			src: encodeURI(song.stream),
 			urls: [encodeURI(song.stream)],
@@ -186,65 +156,65 @@ var AudioPlayer = React.createClass({
 			onend: this.playEnd,
 			buffer: true
 		});
-	},
+	}
 
-	clearSoundObject: function() {
+	clearSoundObject () {
  		if (this.howler) {
 			this.howler.stop();
 			this.howler.unload();
 			this.howler = null;
 		}
- 	},
+ 	}
 
-	initSoundObjectCompleted: function() {
+	initSoundObjectCompleted () {
 		this._play();
 		this.setState({ 
 			duration: this.howler.duration(),
 			isLoading: false
 		});
-	},
+	}
 
-	_play: function() {
+	_play () {
 		this.howler.play();
 		this.stopUpdateCurrentDuration();
 		this.updateCurrentDuration();
 		this.interval = setInterval(this.updateCurrentDuration, 500);
-	},
+	}
 
-	playEnd: function() {
+	playEnd () {
 		if(this.state.currentSongIndex == this.state.songs.length - 1) {
 			this.stop();
 		} else {
 			this.next();
 		}
-	},
+	}
 
-	stop: function() {
+	stop () {
 		this.stopUpdateCurrentDuration();
 		this.setState({ 
 			seek: 0, 
 			isPlaying: false 
 		});
-	},
+	}
 
-	pause: function() {
+	pause () {
 		this.howler.pause();
 		this.stopUpdateCurrentDuration();
-	},
+	}
 
-	prev: function() {
+	prev () {
 		if (this.state.seek > 1 || this.state.currentSongIndex === 0) {
 			this.seekTo(0);
 		} else {
 			this.updateSongIndex(this.state.currentSongIndex - 1);
 		}
-	},
+	}
 
-	next: function() {
+	next () {
 		this.updateSongIndex(this.state.currentSongIndex + 1);
-	},
+	}
 
-	updateSongIndex: function(index) {
+	updateSongIndex (index) {
 		this.setState({ 
 			currentSongIndex: index,
 			duration: 0
@@ -255,45 +225,94 @@ var AudioPlayer = React.createClass({
 		} else {
 			this.stopUpdateCurrentDuration();
 		}
-	},
+	}
 
-	updateCurrentDuration: function() {
+	updateCurrentDuration () {
 		this.setState({ 
 			seek: this.howler.seek() 
 		});
-	},
+	}
 
-	stopUpdateCurrentDuration: function() {
+	stopUpdateCurrentDuration () {
 		clearInterval(this.interval);
-	},
+	}
 
-	seekTo: function(percent) {
-		var seek = this.state.duration * percent;
+	seekTo (percent) {
+		const seek = this.state.duration * percent;
 		this.howler.seek(seek);
 		this.setState({ 
 			seek: seek 
 		});
-	},
+	}
 
-	adjustVolumeTo: function(percent) {
+	adjustVolumeTo (percent) {
 		this.setState({ volume: percent });
 		if (this.howler) {
 			this.howler.volume(percent);
 		}
-	},
+	}
 
-	songCount: function() {
+	songCount () {
 		return this.state.songs ? this.state.songs.length : 0;
-	},
+	}
 
-	getCurrentSongName: function() {
+	getCurrentSongName () {
 		if (this.state.currentSongIndex < 0) {
 			return "";
 		}
-		var song = this.state.songs[this.state.currentSongIndex];
+		const song = this.state.songs[this.state.currentSongIndex];
 		return this.getSongName(song);
  	}
 
-});
+ 	render () {
+		const songCount = this.songCount();
+		let percent = 0;
 
-module.exports = AudioPlayer;
+		if (this.state.seek && this.state.duration) {
+			percent = this.state.seek / this.state.duration;
+		}
+
+		let songName;
+		if (this.songCount() > 1) {
+			const songList = (
+				<SongList ref="songList" 
+					songs={this.state.songs}
+					currentSongIndex={this.state.currentSongIndex} 
+					isPlaying={this.state.isPlaying} isPause={this.state.isPause} 
+					onSongItemClick={this.onSongItemClick}/>
+			);
+			songName = (this.state.currentSongIndex + 1) + ". " + this.getCurrentSongName();
+		} else {
+			songName = this.getCurrentSongName();
+		}
+		
+		return (
+			<div className="audio-player">		
+				<div className="audio-desc-container clearfix">
+					<NameLabel name={songName} />
+					<TimeLabel seek={this.state.seek} duration={this.state.duration}/>
+				</div>
+				<ButtonPanel isPlaying={this.state.isPlaying} 
+					isPause={this.state.isPause}
+					isLoading={this.state.isLoading}
+					currentSongIndex={this.state.currentSongIndex} 
+					songCount={songCount}
+					onPlayBtnClick={this.onPlayBtnClick} 
+					onPauseBtnClick={this.onPauseBtnClick}
+					onPrevBtnClick={this.onPrevBtnClick} 
+					onNextBtnClick={this.onNextBtnClick} />
+				<ProgressBar 
+					shorter={songCount > 1} 
+					percent={percent} 
+					seekTo={this.seekTo} />
+				<VolumeBar 
+					volume={this.state.volume} 
+					adjustVolumeTo={this.adjustVolumeTo} />
+				{/*songList*/}
+			</div>
+		);
+	}
+
+};
+
+export default AudioPlayer;
