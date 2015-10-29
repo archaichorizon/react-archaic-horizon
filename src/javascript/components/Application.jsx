2ef9676dafb248dcad1ece384e2a99a63dd8b718
J Scott Smith <jscsmith@gmail.com>
2015-10-28 23:07:06 -0700

    eslint/cleanup `Application`

--- src/javascript/components/Application.jsx
+++ src/javascript/components/Application.jsx
@@ -1,12 +1,10 @@
-'use strict';
-
 import React, {PropTypes} from 'react';
-
+import _ from 'lodash';
 import Navigation from './Navigation';
 import Loader from './ui/Loader';
 import AudioPlayer from './audioPlayer/AudioPlayer';
 
-import {connectToStores}  from 'fluxible-addons-react';
+import {connectToStores} from 'fluxible-addons-react';
 
 // Store
 import ApplicationStore from '../stores/ApplicationStore';
@@ -17,7 +15,6 @@ import ReleaseStore from '../stores/ReleaseStore';
 
 // Action
 import getReleases from '../actions/getReleases';
-
 
 class Application extends React.Component {
 
@@ -40,7 +37,7 @@ class Application extends React.Component {
             isFetching: this.props.isFetching,
             mood: this.props.mood,
         };
-        const audioPlayer = typeof this.props.playlist !== 'undefined' ? <AudioPlayer playlist={this.props.playlist} mood={this.props.mood}/> : null;
+        const audioPlayer = !_.isUndefined(this.props.playlist) ? <AudioPlayer playlist={this.props.playlist} mood={this.props.mood}/> : null;
         return (
             <div>
                 <Navigation />
@@ -54,7 +51,7 @@ class Application extends React.Component {
     }
 
     render () {
-        return typeof this.props.releases !== 'undefined' ? this.renderApp() : this.renderLoading();
+        return !_.isUndefined(this.props.releases) ? this.renderApp() : this.renderLoading();
     }
 
 }
@@ -63,10 +60,21 @@ Application.contextTypes = {
     executeAction: React.PropTypes.func.isRequired
 };
 
+Application.propTypes = {
+    releases: PropTypes.array,
+    release: PropTypes.object,
+    nav: PropTypes.object,
+    latest: PropTypes.object,
+    isFetching: PropTypes.bool,
+    mood: PropTypes.object,
+    playlist: PropTypes.array,
+    children: PropTypes.object,
+};
+
 const connectedStores = [ApplicationStore, PlayerStore, ReleasesStore, ReleaseStore];
 
-Application = connectToStores(Application, connectedStores, (context, props) => {
-    const releaseStore = context.getStore(ReleaseStore).getState()
+Application = connectToStores(Application, connectedStores, (context) => {
+    const releaseStore = context.getStore(ReleaseStore).getState();
     return {
         route: context.getStore(ApplicationStore).getState(),
         releases: context.getStore(ReleasesStore).getState(),
