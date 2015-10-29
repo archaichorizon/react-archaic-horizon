f9e5b16ebee2a8261a63687ca4e860ed4ae71895
J Scott Smith <jscsmith@gmail.com>
2015-10-29 15:39:33 -0700

    eslint/cleanup `VolumeBar`

--- src/javascript/components/audioPlayer/VolumeBar.jsx
+++ src/javascript/components/audioPlayer/VolumeBar.jsx
@@ -1,95 +1,101 @@
-var React = require('react/addons');
-var classnames = require('classnames');
-import SvgIcon from '../ui/SvgIcon'; 
+import React, {PropTypes} from 'react';
+import cx from 'classnames';
+import SvgIcon from '../ui/SvgIcon';
 
-var uniqueId = 0;
+let uniqueId = 0;
 
-var VolumeBar = React.createClass({
-	
-	getInitialState: function() {
-		return { hide: true };
-	},
+class VolumeBar extends React.Component {
 
-	render: function() {
-		
-		var percent = Math.round(this.props.volume * 100);
-		var style = {top: (100 - percent) + "%"};
-		var toggleIcon = this.props.volume === 0 ? "volume-off" : "volume-up";
+    constructor () {
+        super();
 
-		var audioVolumeBarClasses = classnames({
-			'volume-bar': true,
-  			'volume-bar-hide': this.state.hide
-		});
+        this.state = {
+            hide: true
+        };
 
-		var audioVolumeBarContainerId = "audioVolumeBarContainerId" + ++uniqueId;
-		var toggleBtnId = "toggleBtn" + ++uniqueId;
+        this.toggle = this.toggle.bind(this);
+        this.adjustVolumeTo = this.adjustVolumeTo.bind(this).
+        this.volumeToMax = this.volumeToMax.bind(this);
+        this.volumeToMin = this.volumeToMin.bind(this);
+    }
 
-		return (
-			<div id={audioVolumeBarContainerId} ref="audioVolumeBarContainer" className="volume-bar-container">
-				<button id={toggleBtnId} ref="toggleButton" onClick={this.toggle}>
-					<SvgIcon icon="VOLUME" />
-				</button>
-				{/*<div className="volume-level">Volume: {percent}</div>*/}
-				<div className={audioVolumeBarClasses}>
-					<button className="volume-max" onClick={this.volumeToMax}>
-						100
-					</button>
-					<div ref="audioVolumePercentContainer" className="volume-percent-container" onClick={this.adjustVolumeTo}>
-						<div className="volume-percent" style={style}></div>
-					</div>
-					<button className="volume-mute" onClick={this.volumeToMin}>
-						Mute
-					</button>
-				</div>	
-			</div>
-		);
-	},
+    toggle () {
+        if (this.state.hide) {
+            this.setState({
+                hide: false
+            });
+        } else {
+            this.setState({
+                hide: true
+            });
+        }
 
-	toggle: function() {
+        // when bar open, do nothing if toggle btn press again
+        if (this.isToggleBtnPress) {
+            this.isToggleBtnPress = false;
+            return;
+        }
 
+        var hide = !this.state.hide;
+        if (hide) {
+            return;
+        }
+    }
 
-		if (this.state.hide) {
-			this.setState({
-				hide: false
-			});
-		} else {
-			this.setState({
-				hide: true
-			});
-		}
+    adjustVolumeTo (e) {
+        var container = this.refs.audioVolumePercentContainer.getDOMNode();
+        var containerStartY = container.getBoundingClientRect().top;
+        var percent = (e.clientY - containerStartY) / container.offsetHeight;
+        percent = 1 - percent;
+        // console.log(percent);
+        this.props.adjustVolumeTo(percent);
+    }
 
-		// when bar open, do nothing if toggle btn press again
-		if (this.isToggleBtnPress) {
-			this.isToggleBtnPress = false;
-			return;
-		}
+    volumeToMax () {
+        this.props.adjustVolumeTo(1);
+    }
 
-		var hide = !this.state.hide;
-		if (hide) {
-			return;
-		}
+    volumeToMin () {
+        this.props.adjustVolumeTo(0);
+    }
 
-		//
-		
-	},
+    render () {
+        var percent = Math.round(this.props.volume * 100);
+        var style = {top: `${(100 - percent)}%`};
 
-	adjustVolumeTo: function(e) {
-		var container = this.refs.audioVolumePercentContainer.getDOMNode();
-		var containerStartY = container.getBoundingClientRect().top;
-		var percent = (e.clientY - containerStartY) / container.offsetHeight;	
-		percent = 1 - percent;
-		// console.log(percent);
-		this.props.adjustVolumeTo(percent);
-	},
+        var audioVolumeBarClasses = cx({
+            'volume-bar': true,
+            'volume-bar-hide': this.state.hide
+        });
 
-	volumeToMax: function() {
-		this.props.adjustVolumeTo(1);
-	},
+        var audioVolumeBarContainerId = `audioVolumeBarContainerId ${++uniqueId}`;
+        var toggleBtnId = `toggleBtn ${++uniqueId}`;
 
-	volumeToMin: function() {
-		this.props.adjustVolumeTo(0);
-	}
+        return (
+            <div id={audioVolumeBarContainerId} ref="audioVolumeBarContainer" className="volume-bar-container">
+                <button id={toggleBtnId} ref="toggleButton" onClick={this.toggle}>
+                    <SvgIcon icon="VOLUME" />
+                </button>
+                {/* <div className="volume-level">Volume: {percent}</div> */}
+                <div className={audioVolumeBarClasses}>
+                    <button className="volume-max" onClick={this.volumeToMax}>
+                        100
+                    </button>
+                    <div ref="audioVolumePercentContainer" className="volume-percent-container" onClick={this.adjustVolumeTo}>
+                        <div className="volume-percent" style={style}></div>
+                    </div>
+                    <button className="volume-mute" onClick={this.volumeToMin}>
+                        Mute
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
 
-});
+VolumeBar.propTypes = {
+    volume: PropTypes.number.isRequired,
+    adjustVolumeTo: PropTypes.func.isRequired
+};
 
-module.exports = VolumeBar;
+export default VolumeBar;
