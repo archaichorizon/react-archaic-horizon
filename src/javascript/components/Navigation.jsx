65359dc0a0050b0edf9e14ef1d3753746cb24f0c
J Scott Smith <jscsmith@gmail.com>
2015-10-28 23:07:22 -0700

    eslint/cleanup `Navigation`

--- src/javascript/components/Navigation.jsx
+++ src/javascript/components/Navigation.jsx
@@ -1,22 +1,20 @@
-'use strict';
-
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 
-export default class Navigation extends React.Component{
+export default class Navigation extends React.Component {
     render () {
         var isActive = this.context.history.isActive;
         var homeClass = isActive('/') ? 'selected' : '';
-        var aboutClass = isActive('/about') ? 'selected' : ''
-        var releasesClass = isActive('/releases') ? 'selected' : ''
+        var aboutClass = isActive('/about') ? 'selected' : '';
+        var releasesClass = isActive('/releases') ? 'selected' : '';
         return (
             <header>
                 <h1>Archaic Horizon</h1>
                 <nav>
                     <ul className="nav-links">
-                        <li className={homeClass}><Link to='/'>Home</Link></li>
-                        <li className={aboutClass}><Link to='/about'>About</Link></li>
-                        <li className={releasesClass}><Link to='/releases'>Releases</Link></li>
+                        <li className={homeClass}><Link to="/">Home</Link></li>
+                        <li className={aboutClass}><Link to="/about">About</Link></li>
+                        <li className={releasesClass}><Link to="/releases">Releases</Link></li>
                     </ul>
                 </nav>
             </header>
