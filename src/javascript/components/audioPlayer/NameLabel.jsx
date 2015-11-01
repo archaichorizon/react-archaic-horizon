ccc001a960bad466f62d8d70264dd719ade6e160
J Scott Smith <jscsmith@gmail.com>
2015-11-01 11:34:40 -0800

    update `NameLabel` to also display artist

--- src/javascript/components/audioPlayer/NameLabel.jsx
+++ src/javascript/components/audioPlayer/NameLabel.jsx
@@ -3,13 +3,17 @@ import React, {PropTypes} from 'react';
 class NameLabel extends React.Component {
 	render () {
 		return (
-			<div className="name-label"><strong>{this.props.name}</strong></div>
+			<div className="name-label">
+                <div className="title">{this.props.title}</div>
+                <div className="artist">{this.props.artist}</div>
+            </div>
 		);
 	}
 }
 
 NameLabel.propTypes = {
-    name: PropTypes.string.isRequired,
-}
+    title: PropTypes.string.isRequired,
+    artist: PropTypes.string.isRequired,
+};
 
 export default NameLabel;
