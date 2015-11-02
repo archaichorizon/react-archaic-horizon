5dc287b8d137ca90ca1e45924235241316eaa5f1
J Scott Smith <jscsmith@gmail.com>
2015-11-02 00:40:46 -0800

    update `Releases` markup

--- src/javascript/components/pages/Releases.jsx
+++ src/javascript/components/pages/Releases.jsx
@@ -1,19 +1,25 @@
-'use strict';
-
-import React from 'react';
-import { Link } from 'react-router'
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
 
 class Releases extends React.Component {
 
     render () {
         return (
-            <article>
+            <article className="releases-page">
                 <h1>Releases page.</h1>
-                <ul>
+                <ul className="releases-list">
                     {this.props.releases.map(release => {
                         return (
-                            <li key={release.cat_no}>
-                                <Link to={`/releases/${release.cat_no}`}>{release.title}</Link>
+                            <li className="release" key={release.cat_no}>
+                                <Link to={`/releases/${release.cat_no}`}>
+                                    <div className="cover">
+                                        <img src={release.covers[0].downloads['JPEG Thumb']} alt={release.title} />
+                                    </div>
+                                    <div className="details">
+                                        <h2>{release.creator}</h2>
+                                        <h3>{release.title}</h3>
+                                    </div>
+                                </Link>
                             </li>
                         );
                     })}
@@ -22,6 +28,10 @@ class Releases extends React.Component {
         );
     }
 
+}
+
+Releases.propTypes = {
+    releases: PropTypes.array
 };
 
 export default Releases;
