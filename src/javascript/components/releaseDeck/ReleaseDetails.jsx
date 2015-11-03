0d47e187a40e3a4ea87e6d03418f7566fe1d0500
J Scott Smith <jscsmith@gmail.com>
2015-11-03 00:17:18 -0800

    new classes/markup for `ReleaseDetails`

--- src/javascript/components/releaseDeck/ReleaseDetails.jsx
+++ src/javascript/components/releaseDeck/ReleaseDetails.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SvgIcon from '../ui/SvgIcon';
+import cx from 'classnames';
+import {secondsToTime} from '../../utils/secondsToTime';
 
 export default class ReleaseTracks extends React.Component {
     constructor () {
@@ -15,22 +17,24 @@ export default class ReleaseTracks extends React.Component {
     render () {
         let tracks = this.props.release.tracks.map((track, i) => {
             return (
-                <li key={i}>
-                    <button onClick={this.handleClick.bind(this, i)}>
-                        {track.title}
-                    </button>
-                    <a href={track.stream}>
-                        <SvgIcon icon="DOWNLOAD" />
-                    </a>
+                <li key={'track-' + i} className="track">
+                    <span className="track-title" onClick={this.handleClick.bind(this, i)}>{track.title}</span>
+                    <span className="track-time">{secondsToTime(track.length)}</span>
+                    <a href={track.stream}><SvgIcon icon="DOWNLOAD" /></a>
                 </li>
             );
         });
 
+        const className = cx({
+            'release-tracks': true,
+            'two-column': this.props.release.tracks.length > 10
+        });
+
         return (
-            <article className="release">
+            <article className="release-details">
                 <h1>{this.props.release.title}</h1>
                 <h2>{this.props.release.creator}</h2>
-                <ol className="release-tracks">
+                <ol className={className}>
                     {tracks}
                 </ol>
             </article>
