b76a558690c7f65de548f3f35a116bca9b22295a
J Scott Smith <jscsmith@gmail.com>
2015-11-03 23:57:47 -0800

    cleanup `getRelease` action

--- src/javascript/actions/getRelease.js
+++ src/javascript/actions/getRelease.js
@@ -3,56 +3,56 @@
 import request from'superagent-bluebird-promise';
 
 import fetchingRelease from '../actions/fetchingRelease';
-import setPlaylist from '../actions/setPlaylist';
+// import setPlaylist from '../actions/setPlaylist';
 
-// set release to its catalog 
+// set release to its catalog
 let cache = {};
 
 export default function getRelease (actionContext, payload, done) {
-    
-    payload = payload.toUpperCase();
-    
+
+    let catNum = payload.toUpperCase();
+
     // check cache and return release if true
-    if(cache[payload]) {
-    	// console.log('Cached Response');
-    	actionContext.dispatch('GET_RELEASE', cache[payload]);
+    if(cache[catNum]) {
+        // console.log('Cached Response');
+        actionContext.dispatch('GET_RELEASE', cache[catNum]);
         // actionContext.executeAction(fetchingRelease, false);
-    	done();
-    	return;
+        done();
+        return;
     }
 
     let url;
 
-    switch (payload) {
+    switch (catNum) {
         case 'LATEST':
             url = 'http://api.archaichorizon.com/releases/latest.json';
             break;
         default:
-            url = 'http://api.archaichorizon.com/releases/' + payload + '.json';
+            url = 'http://api.archaichorizon.com/releases/' + catNum + '.json';
             break;
     }
-	
+
     actionContext.executeAction(fetchingRelease, true);
 
-	return request
-		.get(url).then( function(res) {
-			// console.log(res);
-			payload = res.body;
+    return request
+        .get(url).then( function(res) {
+            // console.log(res);
+            catNum = res.body;
 
-			// cache the response
-		    let key = payload.cat_no;
-		    cache[key] = payload;
+            // cache the response
+            let key = catNum.cat_no;
+            cache[key] = catNum;
 
             // SET PLAYLIST
             // set playlist by queueing an album
-            // let playlist = payload.tracks;
+            // let playlist = catNum.tracks;
             // actionContext.dispatch('SET_PLAYLIST', playlist);
 
-			actionContext.dispatch('GET_RELEASE', payload);
+            actionContext.dispatch('GET_RELEASE', catNum);
             actionContext.executeAction(fetchingRelease, false);
-			done();
-		}, function(err) {
-			console.warn(err);
-			done();
-		});
+            done();
+        }, function(err) {
+            console.warn(err);
+            done();
+        });
 }
